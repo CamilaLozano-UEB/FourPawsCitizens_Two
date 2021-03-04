e3c1082b99d0783b4e5812a4f6c6230c9eabb2bd
Nicolas-Pena-Mogollon <nicop4643@gmail.com>
2021-03-04 16:05:29 -0500

    Basic read meyhod using d3

--- javascript/readCSV.js
+++ javascript/readCSV.js
@@ -1,6 +1,5 @@
-var data = [];
-d3.csv('pets-citizens.csv', function(row) {
-	console.log(row);
+d3.dsv(";", "Data/pets-citizens.csv").then(function(data) {
+	console.log(data[1]);
 });
 
-
+/*Promesa fetch api jalar recursos dek server, then funcion que se ejecuta si la función está correcta*/
