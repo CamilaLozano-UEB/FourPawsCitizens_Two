86a7820008e944cb33b7050e849b6c6196b065ab
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-03-08 23:30:31 -0500

    Drag and drop, first part

--- javascript/draganddrop.js
+++ javascript/draganddrop.js
@@ -1,13 +1,33 @@
-function allowDrop(evento) {
-  evento.preventDefault();
-}
+var file = "";
+function dropHandler(evento) {
+    console.log('Fichero(s) arrastrados');
 
-function drag(evento) {
-  evento.dataTransfer.setData("text", evento.target.id);
-}
+    // Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
+    evento.preventDefault();
 
-function drop(evento) {
-  evento.preventDefault();
-  var imagen = evento.dataTransfer.getData("text");
-  evento.target.appendChild(document.getElementById(imagen));
+    if (evento.dataTransfer.items) {
+        // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
+        for (var i = 0; i < evento.dataTransfer.items.length; i++) {
+            // Si los elementos arrastrados no son ficheros, rechazarlos
+            if (evento.dataTransfer.items[i].kind === 'file') {
+                file = evento.dataTransfer.items[i].getAsFile();
+            }
+        }
+    } else {
+        // Usar la interfaz DataTransfer para acceder a el/los archivos
+        for (var i = 0; i < evento.dataTransfer.files.length; i++) {
+        }
+    }
+
+    // Pasar el evento a removeDragData para limpiar
+    removeDragData(evento)
+}
+function removeDragData(evento) {
+    if (evento.dataTransfer.items) {
+        // Use DataTransferItemList interface to remove the drag data
+        evento.dataTransfer.items.clear();
+    } else {
+        // Use DataTransfer interface to remove the drag data
+        evento.dataTransfer.clearData();
+    }
 }
