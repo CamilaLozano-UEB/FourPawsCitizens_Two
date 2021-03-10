b804a4823ee6352860b284d383fe4460557c91bc
JuanaTorres <jvtorresp@unbosque.edu.co>
2021-03-09 21:34:27 -0500

    add in data the new pet Juana Torres

--- javascript/pets.js
+++ javascript/pets.js
@@ -2,7 +2,38 @@ class Manager {
 	constructor(data) {
 		this.pets = data;
 	}
+	static getformCreate(data) {
+		var microchip = document.getElementById("microchip").value;
+		var species = document.getElementById("species").value;
+		var sex = document.getElementById("sex").value;
+		var size = document.getElementById("size").value;
+		var danger = document.getElementById("danger").value;
+		var neighborhood = document.getElementById("neighborhood").value;
+		var race = document.getElementById("race").value;
+		var owner = document.getElementById("owner").value;
+		var address = document.getElementById("address").value;
 
+		data.push({ "microchip": microchip, "species": species, "sex": sex, "size": size, "potentDangerous": danger, "neighborhood": neighborhood, "race": race, "owner": owner, "address": address });
+		return data;
+	}
+	static getformUp(data) {
+		//	var image= document.getElementById("image").value;
+		var microchip = document.getElementById("microchip").value;
+		var race = document.getElementById("race").value;
+		var owner = document.getElementById("owner").value;
+		var address = document.getElementById("address").value;
+		for (var i = 0; i < data.length; i++) {
+			if (data[i]["microchip"] == "microchip") {
+				data[i]["race"] = race;
+				data[i]["owner"] = owner;
+				data[i]["address"] = address;
+				//data[i]["image"] = image;
+			}
+
+		}
+		return data;
+
+	}
 	static createSubLists(data) {
 		var num = parseInt(data.length / 50) + 1;
 		var bidSublist = [];
