37852add8b18233a9cc9f3514e197b574e582647
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-03-15 19:47:21 -0500

    Drag and drop create species

--- javascript/draganddrop.js
+++ javascript/draganddrop.js
@@ -1,33 +1,36 @@
 var file = "";
-function dropHandler(evento) {
-    console.log('Fichero(s) arrastrados');
+function dropHandler(ev) {
+	console.log('Fichero(s) arrastrados');
 
-    // Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
-    evento.preventDefault();
+	// Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
+	ev.preventDefault();
 
-    if (evento.dataTransfer.items) {
-        // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
-        for (var i = 0; i < evento.dataTransfer.items.length; i++) {
-            // Si los elementos arrastrados no son ficheros, rechazarlos
-            if (evento.dataTransfer.items[i].kind === 'file') {
-                file = evento.dataTransfer.items[i].getAsFile();
-            }
-        }
-    } else {
-        // Usar la interfaz DataTransfer para acceder a el/los archivos
-        for (var i = 0; i < evento.dataTransfer.files.length; i++) {
-        }
-    }
+	if (ev.dataTransfer.items) {
+		// Usar la interfaz DataTransferItemList para acceder a el/los archivos)
+		for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+			// Si los elementos arrastrados no son ficheros, rechazarlos
+			if (ev.dataTransfer.items[i].kind === 'file') {
+				file = ev.dataTransfer.items[i].getAsFile();
+				console.log('... file[' + i + '].name = ' + file.webkitRelativePath);
+			}
 
-    // Pasar el evento a removeDragData para limpiar
-    removeDragData(evento)
+		}
+	} else {
+		// Usar la interfaz DataTransfer para acceder a el/los archivos
+		for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+			console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].url);
+		}
+	}
+	// Pasar el evento a removeDragData para limpiar
+	removeDragData(ev)
 }
-function removeDragData(evento) {
-    if (evento.dataTransfer.items) {
-        // Use DataTransferItemList interface to remove the drag data
-        evento.dataTransfer.items.clear();
-    } else {
-        // Use DataTransfer interface to remove the drag data
-        evento.dataTransfer.clearData();
-    }
+function removeDragData(ev) {
+	console.log('Removing drag data')
+	if (ev.dataTransfer.items) {
+		// Use DataTransferItemList interface to remove the drag data
+		ev.dataTransfer.items.clear();
+	} else {
+		// Use DataTransfer interface to remove the drag data
+		ev.dataTransfer.clearData();
+	}
 }
